2e293979e987bdeb1e0c0ca65488cf3dd50721d4
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-06-29 09:20:59 +0530

    calculate

--- HelloooWorld/Program.cs
+++ HelloooWorld/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// See https://aka.ms/new-console-template for more informatio
+using System;
+namespace hellooWorld
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hiii, sakshi here");
+                }
+    }
+}
