b35a8934f828b2409dd21177c7a0653187d0d017
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-06-29 23:36:35 +0530

    update program calculate

--- HelloooWorld/Program.cs
+++ HelloooWorld/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more informatio
+using HelloooWorld;
 using System;
 namespace hellooWorld
 {
@@ -7,6 +8,11 @@ namespace hellooWorld
         static void Main(string[] args)
         {
             Console.WriteLine("hiii, sakshi here");
-                }
+            // object creation of a class
+            calculate calculate = new calculate();
+            calculate.AditionOfTwoNumbers();
+            Console.ReadLine();
+        
+         }
     }
 }
--- HelloooWorld/calculate.cs
+++ HelloooWorld/calculate.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace HelloooWorld
 {
-    internal class calculate
+    public class calculate
     {
-       
+         public void AditionOfTwoNumbers()
+
+        {
+            Console.WriteLine("please enter 1st numbeer"); 
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter 2nd number");
+            int SecondNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("addition is "+firstNum + SecondNum);
+            //int add =firstNum + SecondNum;  
+            //Console.WriteLine("addition is " +add); 
+
+        }
     }
 }
